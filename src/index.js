8d6d4638ab34bf951449af460d172ba351f35a10
hyhong-code <hong961127@gmail.com>
2020-05-11 11:36:17 -0700

    add REST API endpoint for creating tasks

--- src/index.js
+++ src/index.js
@@ -5,19 +5,25 @@ const express = require("express");
 require("./db/mongoose");
 
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 app.use(express.json());
 
 app.post("/users", (req, res) => {
   const user = new User(req.body);
-
   user
     .save()
-    .then((user) => res.status(200).send(res.send(user)))
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+    .then((user) => res.status(201).send(res.send(user)))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then((task) => res.status(201).send(task))
+    .catch((err) => res.status(400).send(err));
 });
 
 const port = process.env.PORT || 3000;
